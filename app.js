411588e07a7159b98f278b07908c67a9049f1bd2
Mariano Hayward <74013144+marianook@users.noreply.github.com>
2022-01-29 15:46:31 -0300

    UPDATES - NAV, RESPONSIVE y 2 IMG's

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-/* When your mouse cursor enter the background, the fading won't pause and keep playing */ 
-$('.carousel').carousel({
-    pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
+/* When your mouse cursor enter the background, the fading won't pause and keep playing */
+$(".carousel").carousel({
+  pause:
+    "false" /* Change to true to make it paused when your mouse cursor enter the background */,
 });
-
-import animate from "https://cdn.jsdelivr.net/npm/animateplus@2/animateplus.js";
-
-animate({
-  elements: ".service-block",
-  duration: 2000,
-  delay: index => index * 100,
-  transform: ["scale(0)", "scale(1)"]
-})
